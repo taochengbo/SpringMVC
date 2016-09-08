9b7a23957849fbf8307d77a196b5c9bc9b476844
liuyh <liuyh@yuminsoft.com>
2016-09-08 18:21:04 +0800

    小修改

--- src/main/java/com/somnus/controller/ResponseBodyBindController.java
+++ src/main/java/com/somnus/controller/ResponseBodyBindController.java
@@ -1,10 +1,16 @@
 package com.somnus.controller;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.somnus.domain.Account;
 
 @Controller
@@ -31,6 +37,33 @@ public class ResponseBodyBindController {
 	    account.setPassword(password);
 	    System.out.println("responsebodybind2:" + account);
         return account;
+    }
+	
+	@RequestMapping(value="/complex", method = {RequestMethod.GET})
+    @ResponseBody
+    public String complex(){
+	    Map<String,List<Integer>> map = new HashMap<String,List<Integer>>();
+	    map.put("one", Arrays.asList(1));
+	    map.put("two", Arrays.asList(1,2));
+	    map.put("three", Arrays.asList(1,2,3));
+	    map.put("four", Arrays.asList(1,2,3,4));
+	    map.put("five", Arrays.asList(1,2,3,4,5));
+	    //TODO
+	    String result = JSON.toJSONString(map);
+        return result;
+    }
+	
+	@RequestMapping(value="/complex2", method = {RequestMethod.GET})
+    @ResponseBody
+    public Map<String,List<Integer>> complex2(){
+	    Map<String,List<Integer>> map = new HashMap<String,List<Integer>>();
+	    map.put("one", Arrays.asList(1));
+	    map.put("two", Arrays.asList(1,2));
+	    map.put("three", Arrays.asList(1,2,3));
+	    map.put("four", Arrays.asList(1,2,3,4));
+	    map.put("five", Arrays.asList(1,2,3,4,5));
+	    //TODO
+        return map;
     }
 	
 }
