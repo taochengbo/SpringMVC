4547c05b320b82e3472a3a9f89e842d1d1b56b76
somnus <928200207@qq.com>
2016-11-28 20:53:59 +0800

    去除警告信息

--- src/main/java/com/somnus/controller/ValidateController.java
+++ src/main/java/com/somnus/controller/ValidateController.java
@@ -1,7 +1,5 @@
 package com.somnus.controller;
 
-import java.security.NoSuchAlgorithmException;
-
 import javax.validation.Valid;
 
 import org.hibernate.validator.constraints.Email;
@@ -9,7 +7,6 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
